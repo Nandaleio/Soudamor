1332ea36eefdc6d2d15e5303ccd9321b38ef9824
Nandaleio <stephGiorgetti@gmail.com>
2022-08-07 21:42:46 +0200

    Deploy Gihub Pages test 2 basehref in Router

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/Soudamor">
         <Header />
 
         <ThemeProvider theme={theme}>
